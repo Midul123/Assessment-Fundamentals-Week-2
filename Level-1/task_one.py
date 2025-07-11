9e57200766c52709ad6b4d32cbe1cda9585ff076
Midul <miduluddin03@gmail.com>
2025-07-11 16:49:45 +0100

    task1 pylint improvements

--- Level-1/task_one.py
+++ Level-1/task_one.py
@@ -1,41 +1,16 @@
+"""
+TASK ONE
+
+"""
+# pylint: disable=too-few-public-methods
+
+
 from datetime import date
-from datetime import datetime
-'''
-## 1. Trainee
-
-You should have a `Trainee` class that has the following attributes:
-
-- `name` - str
-- `email` - str
-- `date_of_birth` - `date` object
-- `assessments` - list[Assessment]
-
-It has methods to:
-
-- `get_age() -> int` - returns the age of the trainee in years
-- `add_assessment(assessment: Assessment) -> None` - adds an `Assessment` to the trainee's list of assessments
-- `get_assessment(name: str) -> Assessment | None` - returns the `Assessment` object that has the name given
-  - Return `None` if not found
-
-
-## 2. Assessment
-
-You should have an `Assessment` class that has the following attributes:
-
-- `name` - str
-- `type` - str
-  - Can only be `multiple-choice`, `technical` or `presentation`. Throw a ValueError if not.
-- score - float (0-100). Throw a ValueError if outside of this range.
-
-## Note
-
-Due to the way that that default arguments are stored in Python, you should not use mutable default arguments in your function signature. This means that **you should not use `[]` or `{}` as default arguments** in your functions as **this will break the tests.**
-
-
-'''
 
 
 class Trainee:
+    """CLASS FOR TRAINEE"""
+
     def __init__(self, name: str, email: str, date_of_birth: date):
         self.name = name
         self.email = email
@@ -43,13 +18,16 @@ class Trainee:
         self.assessments = []
 
     def get_age(self):
+        """METHOD TO GET AGE"""
         today = date.today()
         return today.year - self.date_of_birth.year
 
     def add_assessment(self, assessment):
+        """METHOD TO ADD ASSESSMENT"""
         self.assessments.append(assessment)
 
     def get_assessment(self, name: str):
+        """METHOD TO GET ASSESSMENT"""
         for assessment in self.assessments:
             if assessment.name == name:
                 return assessment
@@ -57,6 +35,8 @@ class Trainee:
 
 
 class Assessment:
+    """CLASS FOR ASSESSMENT"""
+
     def __init__(self, name: str, type: str, score: float):
         types = ["multiple-choice", "technical", "presentation"]
         self.name = name
